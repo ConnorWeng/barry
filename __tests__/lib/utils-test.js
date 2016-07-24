882743655ac266774beff916f77a2b2c9b415858
Connor Weng <foamtea30@gmail.com>
2016-07-24 17:23:43 +0800

    introduce jest and add utils

---
+++ __tests__/lib/utils-test.js
@@ -0,0 +1,20 @@
+'use strict';
+
+jest.unmock('bluebird');
+jest.unmock('../../lib/utils');
+
+const {createPromiseCallback} = require('../../lib/utils');
+
+describe('createPromiseCallback', () => {
+  it('should return a promise could be resolved later', (done) => {
+    jest.useRealTimers();
+    const callback = createPromiseCallback();
+    setTimeout(() => {
+      callback(null, {success: true});
+    }, 1000);
+    callback.promise.then((data) => {
+      expect(data).toEqual({success: true});
+      done();
+    });
+  });
+});
